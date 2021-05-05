5cb79f90787a00d0fd5e5769f183ddfd969b9090
Jesse Patching <jesse.patching@ucalgary.ca>
2021-05-04 20:43:55 -0600

    Typo

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@ set -e -u
 
 pip install -r requirements.txt
 
-if [[ "$FLASK_ENV" == "development "]]; then
+if [[ "$FLASK_ENV" == "development" ]]; then
   flask run --host 0.0.0.0 --port 80
 else
   # Production
