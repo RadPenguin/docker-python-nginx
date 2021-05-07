b2527dfd97a8f0f0f8330a24c31ce0f911f871d5
Jesse Patching <jesse.patching@ucalgary.ca>
2021-05-06 20:33:22 -0600

    Update to .venv settings

--- init.sh
+++ init.sh
@@ -2,14 +2,15 @@
 
 set -e -u
 
-python3 -m venv .venv
-source .venv/bin/activate
-pip install -r requirements.txt
 
 if [[ "$FLASK_ENV" == "development" ]]; then
+  python3 -m venv .venv
+  source .venv/bin/activate
+  pip install -r requirements.txt
   flask run
 else
   # Production
-  nohup /usr/local/bin/uwsgi --uid 1000 --gid 1000 --ini app.ini 2>&1 > /dev/stdout &
+  source .venv/bin/activate
+  nohup /app/.venv/bin/uwsgi --uid 1000 --gid 1000 --ini app.ini 2>&1 > /dev/stdout &
   /usr/sbin/nginx -g 'daemon off;'
 fi
